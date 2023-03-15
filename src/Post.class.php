2396830bc4d589062fbad7ae9949abf44b5f0dc4
RickyYAHA <kasper.skuratowicz159@wp.pl>
2023-03-15 11:44:32 +0100

    esse

---
+++ src/Post.class.php
@@ -0,0 +1,63 @@
+<?php
+class Post {
+    private int $id;
+    private string $filename;
+    private string $timestamp;
+
+    function __construct(int $i, string $f, string $t) {
+        $this->id = $i;
+        $this->filename = $f;
+        $this->timestamp = $t;
+    }
+
+    static function getLast() : Post {
+        global $db;
+        $query = $db->prepare("SELECT * FROM post ORDER BY timestamp DESC LIMIT 1");
+        $query->execute();
+        $result = $query->get_result();
+        $row = $result->fetch_assoc();
+        $p = new Post($row['id'], $row['filename'], $row['timestamp']);
+        return $p; 
+    }
+
+    static function getPage(int $pageNumber = 1, int $postsPerPage = 10) : array {
+        global $db;
+        $query = $db->prepare("SELECT * FROM post ORDER BY timestamp DESC LIMIT ? OFFSET ?");
+        $offset = ($pageNumber-1)*$postsPerPage;
+        $query->bind_param('ii', $postsPerPage, $offset);
+        $query->execute();
+        $result = $query->get_result();
+        $postsArray = array();
+        while($row = $result->fetch_assoc()) {
+            $post = new Post($row['id'],$row['filename'],$row['timestamp']);
+            array_push($postsArray, $post);
+        }
+        return $postsArray;
+    }
+
+    static function upload(string $tempFileName) {
+        $targetDir = "img/";
+        $imgInfo = getimagesize($tempFileName);
+        if(!is_array($imgInfo)) {
+            die("BŁĄD: Przekazany plik nie jest obrazem!");
+        }
+        $randomNumber = rand(10000, 99999) . hrtime(true);
+        $hash = hash("sha256", $randomNumber);
+        $newFileName = $targetDir . $hash . ".webp";
+        if(file_exists($newFileName)) {
+            die("BŁĄD: Podany plik już istnieje!");
+        }
+        $imageString = file_get_contents($tempFileName);
+        $gdImage = @imagecreatefromstring($imageString);
+        imagewebp($gdImage, $newFileName);
+        global $db;
+        $query = $db->prepare("INSERT INTO post VALUES(NULL, ?, ?)");
+        $dbTimestamp = date("Y-m-d H:i:s"); 
+        $query->bind_param("ss", $dbTimestamp, $newFileName);
+        if(!$query->execute())
+            die("Błąd zapisu do bazy danych");
+
+    }
+}
+
+?>
