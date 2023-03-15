94ea1c9c200b156cbc672cf48cf42ee24aeec246
RickyYAHA <kasper.skuratowicz159@wp.pl>
2023-03-15 12:17:55 +0100

    weq

--- src/config.php
+++ src/config.php
@@ -4,7 +4,7 @@ require('./../src/Post.class.php');
 
 $db = new mysqli("localhost", "root", "", "cms");
 
-$loader = new Twig\Loader\FilesystemLoader('./../src/templates');
+$loader = new Twig\Loader\FilesystemLoader('./../src/templates/');
 $twig = new Twig\Environment($loader);
 
 ?>
